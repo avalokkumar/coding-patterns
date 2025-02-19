8b0413dbd705281a1618c704ac9f9e3d155ed9bc
Alok Vishwakarma <avalokkumar@gmail.com>
2025-02-19 07:40:31 +0530

    problems based on greedy approach

---
+++ src/main/java/greedy_technique/GasStation.java
@@ -0,0 +1,56 @@
+package greedy_technique;
+
+//Gas Station
+
+/**There are nn gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+ We have a car with an unlimited gas tank, and it costs cost[i] of gas to travel from the ith station to the next (i+1)th station. We begin the journey with an empty tank at one of the gas stations.
+ Find the index of the gas station in the integer array gas such that if we start from that index we may return to the same index by traversing through all the elements, collecting gas[i] and consuming cost[i].
+ • If it is not possible, return -1.
+ • If there exists such index, it is guaranteed to be unique.
+
+ Constraints:
+ • gas.length == cost.length
+ • 1 ≤ gas.length, cost.length ≤ 10^3
+ • 0 ≤ gas[i], cost[i] ≤10^3
+
+ Example 1:
+ Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+ Output: 3
+ Explanation: The car can start from gas station 3 and travel to the next gas station 4, 0, 1, and 2. The car will return to gas station 3 after consuming all the gas.
+
+ Example 2:
+ Input: gas = [2,3,4], cost = [3,4,3]
+ Output: -1
+ Explanation: The car cannot return to the starting gas station.
+ */
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;    // get the length of the array
+        int total = 0;  // initialize total to 0. total is used to store the total gas
+        int tank = 0;   // initialize tank to 0. tank is used to store the gas in the tank
+        int start = 0;  // initialize start to 0. start is used to store the starting index
+
+        for (int i = 0; i < n; i++) {   // iterate through the array
+            total += gas[i] - cost[i];  // calculate the total gas
+
+            tank += gas[i] - cost[i];   // calculate the gas in the tank
+
+            if (tank < 0) {     // if the gas in the tank is less than 0, then update the tank and start index
+                tank = 0;
+                start = i + 1;
+            }
+        }
+
+        return total >= 0 ? start : -1;     // return the start index if the total gas is greater than or equal to 0, otherwise return -1
+    }
+
+    public static void main(String[] args) {
+        GasStation gasStation = new GasStation();
+        System.out.println(gasStation.canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));    // 3
+        System.out.println(gasStation.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));    // -1
+        System.out.println(gasStation.canCompleteCircuit(new int[]{3, 5, 3, 4}, new int[]{5, 4, 3, 3}));    // 2
+    }
+}
+
+// Time Complexity: O(n) - n is the length of the array
+// Space Complexity: O(1) - no extra space used
---
+++ src/main/java/greedy_technique/JumpGame.java
@@ -0,0 +1,54 @@
+package greedy_technique;
+
+// Jump Game
+
+/**
+ * Statement
+ In a single-player jump game, the player starts at one end of a series of squares, with the goal of reaching the last square.
+ At each turn, the player can take up to s steps towards the last square, where s is the value of the current square.
+
+ For example, if the value of the current square is 3, the player can take either 3 steps, or 2 steps, or 1
+ step in the direction of the last square. The player cannot move in the opposite direction, that is, away from the last square.
+ You have been tasked with writing a function to validate whether a player can win a given game or not.
+ You’ve been provided with the nums integer array, representing the series of squares. The player starts at the first index and, following the rules of the game, tries to reach the last index.
+ If the player can reach the last index, your function returns TRUE; otherwise, it returns FALSE.
+
+ Constraints:
+ 1 ≤ nums.length ≤10^3
+ 0≤ nums[i] ≤10^3
+
+ Example 1:
+ Input: nums = [2,3,1,1,4]
+ Output: true
+ Explanation: The player can reach the last index by moving from index 0 to index 1, and then to the last index.
+
+ Example 2:
+ Input: nums = [3,2,1,0,4]
+ Output: false
+ Explanation: The player cannot reach the last index.
+ */
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;    // get the length of the array
+        int max = 0;    // initialize max to 0. max is used to store the maximum index
+
+        for (int i = 0; i < n; i++) {   // iterate through the array
+            if (i > max) {  // if the index is greater than max, then return false
+                return false;
+            }
+
+            max = Math.max(max, i + nums[i]);   // update the max value with the maximum of max and i + nums[i]
+        }
+
+        return true;    // return true
+    }
+
+    public static void main(String[] args) {
+        JumpGame jumpGame = new JumpGame();
+        System.out.println(jumpGame.canJump(new int[]{2, 3, 1, 1, 4}));    // true
+        System.out.println(jumpGame.canJump(new int[]{3, 2, 1, 0, 4}));    // false
+    }
+}
+
+// Time Complexity: O(n) - n is the length of the array
+// Space Complexity: O(1) - no extra space used
